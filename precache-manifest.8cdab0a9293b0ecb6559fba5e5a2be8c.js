1a76303e6c97c9ccc6a057de58bd29df1787c062
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-11-21 16:37:39 +0400

    Updates

---
+++ precache-manifest.8cdab0a9293b0ecb6559fba5e5a2be8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e121426b7a6ff44567b05075adc943e",
+    "url": "/customizable-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "635f3c0a0d476b6925fd",
+    "url": "/customizable-tic-tac-toe/static/css/main.3fab589f.chunk.css"
+  },
+  {
+    "revision": "a63f7a6dde2c3b7f1f27",
+    "url": "/customizable-tic-tac-toe/static/js/2.3cad7e26.chunk.js"
+  },
+  {
+    "revision": "635f3c0a0d476b6925fd",
+    "url": "/customizable-tic-tac-toe/static/js/main.a0e343b8.chunk.js"
+  },
+  {
+    "revision": "8a04dc3687a53130b4c1",
+    "url": "/customizable-tic-tac-toe/static/js/runtime-main.e67c885d.js"
+  }
+]);
